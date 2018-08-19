8c2493277b299b4cf37c532206ebf011630389e1
Iain G <iaingill.law@gmail.com>
2018-08-19 01:32:37 -0400

    finalised logic added simple css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,61 @@
 
 
 var posibleChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var wins = 0;
 var losses = 0;
-var remainingGuesses = 10;
+var remainingGuesses = 9;
 var guessedLetters = [];
 var userGuess = 0;
 var randomLetter = posibleChoices[Math.floor(Math.random() * posibleChoices.length)];
 
 
-
-function consoleTest() {
-    console.log("wins: " + wins);
-    console.log("losses: " + losses);
-    console.log("guessed Letters: " + guessedLetters);
-    console.log("User guess: " + userGuess);
-    console.log("Random letter: " + randomLetter);
-}
-
 function reset() {
-    remainingGuesses = 10;
+    remainingGuesses = 9;
     randomLetter = posibleChoices[Math.floor(Math.random() * posibleChoices.length)];
+    guessedLetters = []
 }
 
-document.onkeyup = function(event) {
+
+document.onkeyup = function (event) {
 
     var userGuess = event.key;
 
+    if (posibleChoices.indexOf(userGuess) >= 0) {
 
-    if ((userGuess === randomLetter)) {
-        wins++;
-        reset();
+        if (guessedLetters.includes(userGuess)) {
+            alert("You already tried that one");
+            remainingGuesses++;
+        }
+
+        if ((userGuess === randomLetter)) {
+            wins++;
+            guessedLetters.push(userGuess);
+            reset();
+        }
+
+        if ((userGuess !== randomLetter)) {
+            remainingGuesses--;
+            guessedLetters.push(userGuess);
+        }
+
+        if ((remainingGuesses === 0)) {
+            losses++;
+            guessedLetters.push(userGuess);
+            reset();
+        }
+
+        var html =
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + losses + "</p>" +
+            "<p>Guesses Remaining: " + remainingGuesses + "</p>" +
+            "<p>Choices so far: " + guessedLetters + " " + "</p>";
+
+        document.querySelector("#game").innerHTML = html;
+
+
     }
-
-    if ((userGuess !== randomLetter)) {
-        remainingGuesses--;
-    }
-
-    if ((remainingGuesses === 0)) {
-        losses++;
-        reset();
-    }
-
-    var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>The computer chose: " + randomLetter + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>Guesses: " + remainingGuesses + "</p>";
-
-    document.querySelector("#game").innerHTML = html;
-
-    consoleTest()
 
 }
-
 
